da2be80702a94144221a15b42c59eb4bad7ed81a
Russell Smith <russellsmith@MacBook-Pro.local>
2020-07-24 11:59:43 -0400

    add functionality for client to join a class

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,12 +1,59 @@
-const { find } = require("./user.model");
+const User = require("./user.model");
+const Classes = require("../class/class.model");
 
 async function getAllUsers(req, res) {
   try {
-    let users = await find();
+    let users = await User.find();
     res.status(200).json(users);
   } catch (e) {
     res.status(500).json({ message: "uable to get users" });
   }
 }
 
-module.exports = { getAllUsers };
+async function userJoinClass(req, res) {
+  const { user_id, class_id } = req.params;
+  if (!user_id || !class_id) {
+    return res
+      .status(400)
+      .json({ message: "missing params user_id and/or class_id" });
+  }
+
+  try {
+    const [user, classes] = await Promise.all([
+      User.findById(user_id),
+      Classes.findById(class_id)
+    ]);
+
+    if (!user) {
+      return res.status(400).json({ message: "user does not exist" });
+    }
+
+    if (!classes) {
+      return res.status(400).json({ message: "class does not exist" });
+    }
+
+    // check to see if class is already fulled up
+    if (classes.numberOfRegisteredAttendees === classes.maxClassSize) {
+      return res
+        .status(400)
+        .json({ message: "already reach the max capacity for class" });
+    }
+
+    // add user to specified class
+    await User.joinClass(user_id, class_id);
+
+    // update number of registered attendees joined
+    const joinedClass = await Classes.update(class_id, {
+      numberOfRegisteredAttendees: classes.numberOfRegisteredAttendees + 1
+    });
+
+    res.status(200).json(joinedClass);
+  } catch (e) {
+    if (e.code === "SQLITE_CONSTRAINT") {
+      return res.status(400).json({ message: "user has already joined class" });
+    }
+    res.status(500).json({ message: "uable to join class" });
+  }
+}
+
+module.exports = { getAllUsers, userJoinClass };
